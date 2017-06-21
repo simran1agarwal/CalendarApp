ae607c0e975b1aee8bffaa11175ddf155c2b30f9
simran1agarwal <simranagarwal1912@gmail.com>
2017-06-21 22:52:45 +0530

    Months added to calendar

--- src/screen/calendar.js
+++ src/screen/calendar.js
@@ -5,6 +5,8 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
+import Moment from 'moment';
+import MonthItem from './src/month_item.js';
 
 class CalendarsScreen extends React.Component {
     static navigationOptions = {
@@ -12,8 +14,22 @@ class CalendarsScreen extends React.Component {
     };
     render(){
         return(
-            <Text>Calendars</Text>
+            <View>
+                <Text>Calendars</Text>
+                {this.months()}
+            </View>
         );
     }
+    months(){
+        var monthsItem = [];
+        for(i=0;i<12;i++){
+           var month = Moment().add(i, 'months').format('MMMM YYYY');
+           monthsItem.push(
+            <MonthItem key={i} month={month} monthsUntil={i} />
+           )
+        }
+
+        return monthsItem
+      }
 }
 export default CalendarsScreen;
