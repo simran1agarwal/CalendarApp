980e98fc173d42140a1f49df56f88469e3f70908
simran1agarwal <simranagarwal1912@gmail.com>
2017-07-03 11:24:17 +0530

    Calendar made in table format

--- src/screen/calendar.js
+++ src/screen/calendar.js
@@ -3,6 +3,7 @@ import {
     Text, View, TouchableOpacity, StyleSheet, ScrollView,FlatList,SectionList,ListView,
 } from 'react-native';
 import Moment from 'moment';
+import { Table, TableWraper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 import MonthItem from './src/month_item.js';
 
@@ -20,8 +21,13 @@ class CalendarsScreen extends React.Component {
                         renderItem={
                             ({item}) =>
                                 <View style={styles.footer}>
-                                    <Text>{item.title}</Text>
+                                    <View>
+                                        {item.titleh}
+                                        {item.titled}
+                                    </View>
+
                                 </View>
+
                         }
 
                     />
@@ -32,7 +38,7 @@ class CalendarsScreen extends React.Component {
         var dateList = [];
         var j=0;
         var k= 30;
-        for(i=1;i<10;i++){
+        /*for(i=1;i<10;i++){
             dateList.push(
                 <Text style={styles.dayContainer}>
                     <Text key={i} style={styles.dayText}>0{i}          </Text>
@@ -45,39 +51,84 @@ class CalendarsScreen extends React.Component {
                      <Text key={i} style={styles.dayText}>{i}          </Text>
                  </Text>
              );
-        }
+        }*/
         var weekdayList = [];
-
-        weekdayList.push(
+        var weekday = [];
+        const tableHead = ['S','M', 'T', 'W', 'T','F','S'];
+        const tableData = [
+            ['1', '2', '3', '4','5','6','7'],
+            ['8','9','10','11','12','13','14'],
+            ['15','16','17','18','19','20','21'],
+            ['22','23','24','25','26','27','28'],
+            ['29','30','31','','','',''],
+        ];
+        /*weekdayList.push(
             <Text style={styles.weekdayContainer}>
-                <Text style={styles.dayText}>S            </Text>
-                <Text style={styles.dayText}>M            </Text>
-                <Text style={styles.dayText}>T            </Text>
-                <Text style={styles.dayText}>W            </Text>
-                <Text style={styles.dayText}>T            </Text>
-                <Text style={styles.dayText}>F            </Text>
-                <Text style={styles.dayText}>S            </Text>
+                <Text style={styles.dayText}>S</Text>
+                <Text style={styles.dayText}>M</Text>
+                <Text style={styles.dayText}>T</Text>
+                <Text style={styles.dayText}>W</Text>
+                <Text style={styles.dayText}>T</Text>
+                <Text style={styles.dayText}>F</Text>
+                <Text style={styles.dayText}>S</Text>
             </Text>
+        );*/
+        weekdayList.push(
+            <Table>
+                <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+            </Table>
         );
+        dateList.push(
+            <Table>
+                 <Rows data={tableData} style={styles.row} textStyle={styles.text}/>
+            </Table>
+                );
+        weekday.push(
+
+                <Table>
+                    <Row data={tableHead} style={styles.head} textStyle={styles.text}/>
+                    <Rows data={tableData} style={styles.row} textStyle={styles.text}/>
+                </Table>
+
+        )
+        var month0 = Moment().add(0, 'months').format('MMMM YYYY');
+        var month1 = Moment().add(1, 'months').format('MMMM YYYY');
+        var month2 = Moment().add(2, 'months').format('MMMM YYYY');
+        var month3 = Moment().add(3, 'months').format('MMMM YYYY');
+        var month4 = Moment().add(4, 'months').format('MMMM YYYY');
+        var month5 = Moment().add(5, 'months').format('MMMM YYYY');
+        var month6 = Moment().add(6, 'months').format('MMMM YYYY');
+        var month7 = Moment().add(7, 'months').format('MMMM YYYY');
+        var month8 = Moment().add(8, 'months').format('MMMM YYYY');
+        var month9 = Moment().add(9, 'months').format('MMMM YYYY');
+        var month10 = Moment().add(10, 'months').format('MMMM YYYY');
+        var month11 = Moment().add(11, 'months').format('MMMM YYYY');
+        var month12 = Moment().add(12, 'months').format('MMMM YYYY');
+        var month13 = Moment().add(13, 'months').format('MMMM YYYY');
+        var month14 = Moment().add(14, 'months').format('MMMM YYYY');
+        var month15= Moment().add(15, 'months').format('MMMM YYYY');
+        var month16 = Moment().add(16, 'months').format('MMMM YYYY');
+
+
 
         return([
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(0, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(1, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(2, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(3, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(4, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(5, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(6, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(7, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(8, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(9, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(10, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(11, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(12, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(13, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(14, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(15, 'months').format('MMMM YYYY')},
-            {data: [{title:weekdayList},{title:dateList}],key: Moment().add(16, 'months').format('MMMM YYYY')},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month0},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month1},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month2},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month3},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month4},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month5},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month6},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month7},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month8},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month9},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month10},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month11},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month12},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month13},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month14},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month15},
+            {data: [{titleh:weekdayList},{titled:dateList}],key: month16},
 
 
 
@@ -112,7 +163,8 @@ const styles = StyleSheet.create({
     footer:{
         flex:5,
         flexDirection:'row',
-
+        alignContent:'center',
+        justifyContent:'center',
 
         borderColor:'red',
         borderWidth:4,
@@ -120,9 +172,10 @@ const styles = StyleSheet.create({
     weekdayContainer:{
         flex:1,
         flexDirection:'row',
+        justifyContent:'space-around',
+        alignContent:'center',
 
         borderColor:'blue',
-        backgroundColor:'yellow',
         borderWidth:4,
     },
     monthText: {
@@ -145,6 +198,9 @@ const styles = StyleSheet.create({
         fontSize:15,
         fontFamily:'Calibiri',
     },
+    head: { width:340, height: 30, backgroundColor: 'yellow', },
+      text: { marginLeft: 5 },
+      row: { flex:1, width:340, height: 30, backgroundColor:'green' }
 
 });
 
